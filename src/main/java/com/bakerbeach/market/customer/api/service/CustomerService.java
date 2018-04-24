82c0dcd07e88bd5b1435b1c43e4311ed412743a0
achimwolf <achim.wolf@baker-beach.com>
2018-04-24 13:41:30 +0200

    version 3.1.1

--- src/main/java/com/bakerbeach/market/customer/api/service/CustomerService.java
+++ src/main/java/com/bakerbeach/market/customer/api/service/CustomerService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.bakerbeach.market.core.api.model.Customer;
+import com.bakerbeach.market.customer.api.model.ResetPasswordToken;
 
 public interface CustomerService {
 
@@ -20,8 +21,9 @@ public interface CustomerService {
 	Customer register(String email, String password, String shopCode) throws CustomerServiceException;
 
 	Customer register(String email, String password, List<String> shopCode) throws CustomerServiceException;
-	
-	Customer register(String email, String password, List<String> shopCode, String firstName, String lastName) throws CustomerServiceException;
+
+	Customer register(String email, String password, List<String> shopCode, String firstName, String lastName)
+			throws CustomerServiceException;
 
 	Boolean checkPassword(Customer customer, String password) throws CustomerServiceException;
 
@@ -30,5 +32,11 @@ public interface CustomerService {
 	void renewPassword(String email, String shopCode) throws CustomerServiceException;
 
 	Customer createAnonymousCustomer();
+
+	String createResetPasswordToken(String email, String shopCode) throws CustomerServiceException;
+
+	ResetPasswordToken getResetPasswordToken(String tokenId) throws CustomerServiceException;
+
+	void deleteResetPasswordToken(String id) throws CustomerServiceException;
 
 }
